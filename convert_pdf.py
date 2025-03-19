dae3dc6e2be3edc5ea6727d613b650e1f1d2171b
zhangpingrui <1050117526@qq.com>
2025-03-19 23:41:13 +0800

    update

--- convert_pdf.py
+++
@@ -1,8 +0,0 @@
-from pdf2image import convert_from_path
-
-# 将 PDF 转换为图片
-images = convert_from_path("image_new/real_world_new.pdf", dpi=300)
-
-# 保存图片
-for i, image in enumerate(images):
-    image.save(f"page_{i+1}.png", "PNG")
